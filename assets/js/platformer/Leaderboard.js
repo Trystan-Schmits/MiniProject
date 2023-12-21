c1c1c9b03ba5b0befd8826ade53d1e54a7bb7b0f
Trystan-Schmits <trystan.yt.2@gmail.com>
2023-12-21 21:37:01 +0000

    organization

--- assets/js/platformer/Leaderboard.js
+++ assets/js/platformer/Leaderboard.js
@@ -15,93 +15,92 @@ export class Leaderboard extends LocalStorage { //create a class with access to 
     }
 
     showLeaderboard(){
-            this.loadAll();
-
-            const id = document.getElementById("gameOver");
-            id.hidden = false;
-            // Hide game canvas and controls
-            document.getElementById('canvasContainer').style.display = 'none';
-            document.getElementById('controls').style.display = 'none';
-            document.getElementById('score').style.display = 'none';
+      this.loadAll();
+      const id = document.getElementById("gameOver");
+      id.hidden = false;
+      // Hide game canvas and controls
+      document.getElementById('canvasContainer').style.display = 'none';
+      document.getElementById('controls').style.display = 'none';
+      document.getElementById('score').style.display = 'none';
         
-          // Create and display leaderboard section
-          const leaderboardSection = document.createElement('div');
-          leaderboardSection.id = 'leaderboardSection';
-          leaderboardSection.innerHTML = '<h1 style="text-align: center; font-size: 18px;">Leaderboard </h1>';
-          document.querySelector(".page-content").appendChild(leaderboardSection)
-          // document.body.appendChild(leaderboardSection);
+      // Create and display leaderboard section
+      const leaderboardSection = document.createElement('div');
+      leaderboardSection.id = 'leaderboardSection';
+      leaderboardSection.innerHTML = '<h1 style="text-align: center; font-size: 18px;">Leaderboard </h1>';
+      document.querySelector(".page-content").appendChild(leaderboardSection)
+      // document.body.appendChild(leaderboardSection);
         
-          var playerScoresArray;
-          const playerScores = this[this.keys.leaderboard];
-          if (playerScores == ""){
-            console.log("no data"); 
-            playerScoresArray=[];
-          } else{
-          playerScoresArray = playerScores.slice(0,-1).split(";");
-          }
+      var playerScoresArray;
+      const playerScores = this[this.keys.leaderboard];
+      if (playerScores == ""){
+        console.log("no data"); 
+        playerScoresArray=[];
+      } else{
+      playerScoresArray = playerScores.slice(0,-1).split(";");
+      }
 
-          var table = document.createElement("table"); // create table
-          table.style.margin = "auto";
+      var table = document.createElement("table"); // create table
+      table.style.margin = "auto";
 
-          var tableHead = document.createElement("tHead"); //create table header
-          var tableHeadRow = document.createElement("tr");
-          tableHead.append(tableHeadRow);
-          var tableTitle1 = document.createElement("th");
-          tableTitle1.innerText = "Run";
-          tableHeadRow.append(tableTitle1);
-          var tableTitle2 = document.createElement("th");
-          tableTitle2.innerText = "Name";
-          tableHeadRow.append(tableTitle2);
-          var tableTitle3 = document.createElement("th");
-          tableTitle3.innerText = "Time";
-          tableHeadRow.append(tableTitle3);
-          table.append(tableHead);
+      var tableHead = document.createElement("tHead"); //create table header
+      var tableHeadRow = document.createElement("tr");
+      tableHead.append(tableHeadRow);
+      var tableTitle1 = document.createElement("th");
+      tableTitle1.innerText = "Run";
+      tableHeadRow.append(tableTitle1);
+      var tableTitle2 = document.createElement("th");
+      tableTitle2.innerText = "Name";
+      tableHeadRow.append(tableTitle2);
+      var tableTitle3 = document.createElement("th");
+      tableTitle3.innerText = "Time";
+      tableHeadRow.append(tableTitle3);
+      table.append(tableHead);
 
-          var tableBody = document.createElement("tBody"); //create table body
-          table.append(tableBody);
-          for (let i =0; i<playerScoresArray.length; i++) {
-            var row = document.createElement("tr");
-            var rank = document.createElement("td");
-            rank.id = String(i)+"r";
-            rank.innerText = String(i+1);
-            var name = document.createElement("td");
-            name.id = String(i)+"n";
-            name.innerText = String(playerScoresArray[i].split(",")[0]);
-            var score = document.createElement("td");
-            score.id = String(i)+"s";
-            score.innerText = String(playerScoresArray[i].split(",")[1]);
-            row.append(rank);
-            row.append(name);
-            row.append(score);
-            tableBody.append(row);  
-          }
-          leaderboardSection.appendChild(table); //apend table
+      var tableBody = document.createElement("tBody"); //create table body
+      table.append(tableBody);
+      for (let i =0; i<playerScoresArray.length; i++) {
+        var row = document.createElement("tr");
+        var rank = document.createElement("td");
+        rank.id = String(i)+"r";
+        rank.innerText = String(i+1);
+        var name = document.createElement("td");
+        name.id = String(i)+"n";
+        name.innerText = String(playerScoresArray[i].split(",")[0]);
+        var score = document.createElement("td");
+        score.id = String(i)+"s";
+        score.innerText = String(playerScoresArray[i].split(",")[1]);
+        row.append(rank);
+        row.append(name);
+        row.append(score);
+        tableBody.append(row);  
+      }
+      leaderboardSection.appendChild(table); //apend table
 
-          const tBody = table.tBodies[0]; //table sorting when clicking on header
-          const rows = Array.from(tBody.rows);
-          const headerCells = table.tHead.rows[0].cells;
-          for (const th of headerCells) {
-            const cellIndex = th.cellIndex;
-            th.addEventListener("click", () => {
-              rows.sort((tr1, tr2) => {
-                const tr1Text = tr1.cells[cellIndex].textContent;
-                const tr2Text = tr2.cells[cellIndex].textContent;
-                return tr1Text.localeCompare(tr2Text, undefined, { numeric: true, sensitivity: "base" });
-              });
-              tBody.append(...rows);
-            });
-          }
-
-          var clearButton = document.createElement("button"); //button for clearing data
-          clearButton.style.margin = "auto";
-          clearButton.innerText = "clear leaderboard"
-          clearButton.addEventListener("click",()=>{
-            this[this.keys.leaderboard] = "";
-            this.save(this.keys.leaderboard);
-            table.remove();
-            clearButton.remove();
+      const tBody = table.tBodies[0]; //table sorting when clicking on header
+      const rows = Array.from(tBody.rows);
+      const headerCells = table.tHead.rows[0].cells;
+      for (const th of headerCells) {
+        const cellIndex = th.cellIndex;
+        th.addEventListener("click", () => {
+          rows.sort((tr1, tr2) => {
+            const tr1Text = tr1.cells[cellIndex].textContent;
+            const tr2Text = tr2.cells[cellIndex].textContent;
+            return tr1Text.localeCompare(tr2Text, undefined, { numeric: true, sensitivity: "base" });
           });
-          leaderboardSection.appendChild(clearButton); //apend button
+          tBody.append(...rows);
+        });
+      }
+
+      var clearButton = document.createElement("button"); //button for clearing data
+      clearButton.style.margin = "auto";
+      clearButton.innerText = "clear leaderboard"
+      clearButton.addEventListener("click",()=>{
+        this[this.keys.leaderboard] = "";
+        this.save(this.keys.leaderboard);
+        table.remove();
+        clearButton.remove();
+      });
+      leaderboardSection.appendChild(clearButton); //apend button
     }
 
     // Function to stop the timer
@@ -110,25 +109,24 @@ export class Leaderboard extends LocalStorage { //create a class with access to 
     }
 
         // Function to update and display the timer
-     updateTimer() {
-        const id = document.getElementById("gameOver");
+    updateTimer() {
+      const id = document.getElementById("gameOver");
+      if (id.hidden == false) {
+          this.stopTimer();
+          this.time=-1;
+      }
+      this.time+=1/GameEnv.frameRate.toFixed(2); // Increment time (you can adjust this based on your game logic)
 
-        if (id.hidden == false) {
-            this.stopTimer();
-            this.time=-1;
-        }
-       this.time+=1/GameEnv.frameRate.toFixed(2); // Increment time (you can adjust this based on your game logic)
-
-       // Display the updated time in the span element with id 'timeScore'
-       const timeScoreElement = document.getElementById('timeScore');
-       if (timeScoreElement) {
-           timeScoreElement.textContent = String(this.time.toFixed(2)); // Update the displayed time
-       }
+      // Display the updated time in the span element with id 'timeScore'
+      const timeScoreElement = document.getElementById('timeScore');
+      if (timeScoreElement) {
+          timeScoreElement.textContent = String(this.time.toFixed(2)); // Update the displayed time
+      }
     }
 
 
     // Function to start the timer
-     startTimer() {
+    startTimer() {
        // Start the timer interval, updating the timer every second (1000 milliseconds)
        this.timerInterval = setInterval(this.updateTimer.bind(this), 1000/GameEnv.frameRate);
     }
